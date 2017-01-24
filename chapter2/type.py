1d2a148ef89d6c2df90fea8e63db6b502132be86
arturdziocha <arturdziocha@yahoo.com>
2017-01-24 08:57:56 +0100

    dsfsfsdf
    zcxxzc

---
+++ chapter2/type.py
@@ -0,0 +1,22 @@
+print(type("Hello world"))
+print(type(27))
+print("Hello, World")
+print(type(3.2))
+print(type("17"))
+print(type("3.2"))
+print(type('This is a string.') )
+print(type("And so is this.") )
+print(type("""and this.""") )
+print(type('''and even this...''') )
+print('''"Oh no", she exclaimed, "Ben's bike is broken!"''')
+
+message = """This message will
+span several
+lines."""
+print(message)
+
+print("""This message will span
+several lines
+of the text.""")
+print(42000)
+print(42,000)
