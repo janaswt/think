ddb72689d2e10430697860126a6cb57f3b5bfe6c
arturdziocha <arturdziocha@yahoo.com>
2017-01-26 00:28:33 +0100

    asdasdsadasd

--- chapter2/type2.py
+++ chapter2/type2.py
@@ -3,9 +3,10 @@ print(3.14, int(3.14))
 print(3.9999, int(3.9999))       # This doesn't round to the closest int!
 print(3.0, int(3.0))
 print(-3.999, int(-3.999))        # Note that the result is closer to zero
-
+print("Dupa")
 print("2345", int("2345"))        # parse a string to produce an int
 print(17, int(17))                # int even works on integers
+
 
 
 print(float("123.45"))
