4cee8d4a02478cfef8d76e672056f1247d26459f
arturdziocha <arturdziocha@yahoo.com>
2017-02-02 16:05:53 +0100

    ft

---
+++ chapter12/concatenation.py
@@ -0,0 +1,6 @@
+fruit = ["apple", "orange", "banana", "cherry"]
+print([1, 2] + [3, 4])
+print(fruit + [6, 7, 8, 9])
+
+print([0] * 4)
+print([1, 2, ["hello", "goodbye"]] * 2)
---
+++ chapter12/method.py
@@ -0,0 +1,10 @@
+mylist = []
+mylist.append(5)
+mylist.append(27)
+mylist.append(3)
+mylist.append(12)
+print(mylist)
+
+dd = mylist.sort()   #probably an error
+print(dd)
+print(mylist)
